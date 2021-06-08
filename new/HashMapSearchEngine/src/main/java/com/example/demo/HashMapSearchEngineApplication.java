4d6d6772ddb2137fdf00e9b867303d6fab307d6a
shafkat2 <shafkat.waheed@northsouth.edu>
2021-06-09 04:19:22 +0600

    updated the values

--- new/HashMapSearchEngine/src/main/java/com/example/demo/HashMapSearchEngineApplication.java
+++ new/HashMapSearchEngine/src/main/java/com/example/demo/HashMapSearchEngineApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @SpringBootApplication
-public class HashMapSearchEngineApplication {
+public class HashMapSearchEngineApplication extends SpringBootServletInitializer {
 	
 
 	public static void main(String[] args) {
